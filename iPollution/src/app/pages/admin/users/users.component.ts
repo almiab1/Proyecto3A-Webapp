b0faad1eacbc7cdb48efd897e7f7c457936696c6
almiab1 <almiab1@epsg.upv.es>
2019-11-15 12:47:56 +0100

    anaydido disenyo general webapp y modificado estructura directorio disenyos

--- iPollution/src/app/pages/admin/users/users.component.ts
+++ iPollution/src/app/pages/admin/users/users.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { LogicaDeNegocioFake } from './../../../core/services/LogicaDeNegocioFake.service';
+
 
 @Component({
   selector: 'app-users',
@@ -7,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll: IonInfiniteScroll;
+
+  constructor(
+    server = LogicaDeNegocioFake) {
+
+     }
 
   ngOnInit() {
 
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+      const datos = [
+        'Alex',
+        'Pepe',
+        'Juan'
+    ];
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      // tslint:disable-next-line: triple-equals
+      if (datos.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 
 }
