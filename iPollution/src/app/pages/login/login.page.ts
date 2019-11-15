b9c906713414019b91369da0d7d220400272b7c2
oscarblanq <oscarblanq@gmail.com>
2019-11-15 13:01:01 +0100

    Login fake implementado

--- iPollution/src/app/pages/login/login.page.ts
+++ iPollution/src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ModalController, NavParams} from "@ionic/angular";
+import {LoadingController, ModalController, NavParams, ToastController} from '@ionic/angular';
+import {delay} from "rxjs/operators";
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,58 @@ import {ModalController, NavParams} from "@ionic/angular";
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor(public modalCtrl: ModalController) { }
-
+  constructor(public modalCtrl: ModalController,
+              private toastCtrl: ToastController,
+              public loadingController: LoadingController,
+              private router: Router) { }
+  email: string;
+  password: string;
+  loading: any;
   ngOnInit() {
   }
   cerrarModal = () => {
     this.modalCtrl.dismiss({
-      'dismissed': true
+      dismissed: true
     });
+  }
+  onSubmitTemplate = () => {
+    console.log('Form submit');
+    if (this.password === '1234') {
+      this.loginCorrecto();
+    } else {
+      this.loginIncorrecto();
+    }
+  }
+  presentarToast = async (mensaje: string, color: string)  => {
+    const toast = await this.toastCtrl.create({
+      message: mensaje,
+      duration: 2000,
+      color,
+      position: 'top'
+    });
+    toast.present();
+  }
+  loginIncorrecto = () => {
+    this.presentarToast('Correo electronico y contraseña no validos', 'danger');
+  }
+  loginCorrecto = () => {
+    this.presentarLoading();
+    setTimeout(() => {
+      this.loading.dismiss();
+    }, 2000);
+    setTimeout(this.cerrarModalConLogin, 2250);
+
+  }
+  cerrarModalConLogin = () => {
+    this.modalCtrl.dismiss({
+      dismissed: true
+    });
+    this.presentarToast('Hola, admin', 'success');
+  }
+  presentarLoading = async () => {
+    this.loading = await this.loadingController.create({
+      message: 'Entrando',
+    });
+    return this.loading.present();
   }
 }
