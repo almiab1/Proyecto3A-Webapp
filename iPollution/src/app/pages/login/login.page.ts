bcacc78ad6c308e68da1ea6ceec2ddf3dab7d8af
oscarblanq <oscarblanq@gmail.com>
2019-11-20 10:28:26 +0100

    ruta login.service cambiada

--- iPollution/src/app/pages/login/login.page.ts
+++ iPollution/src/app/pages/login/login.page.ts
@@ -5,8 +5,9 @@
 import { Component, OnInit } from '@angular/core';
 import {LoadingController, ModalController, ToastController} from '@ionic/angular';
 import {Router} from '@angular/router';
-import {LoginService} from '../../services/login.service';
+
 import { Platform } from '@ionic/angular';
+import {LoginService} from '../../core/services/login.service';
 
 @Component({
   selector: 'app-login',
