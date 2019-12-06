b0dc8a4d41b08f30401b5055d7fa86063496c082
almiab1 <almiab1@epsg.upv.es>
2019-12-06 20:32:00 +0100

    añadido servicio mapa y receptorBle y puestos en app.module

--- iPollution/src/app/app.module.ts
+++ iPollution/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReceptorBLE } from './core/services/ReceptorBle.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,6 +12,11 @@ import { AppRoutingModule } from './app-routing.module';
 import {SharedModule} from './components/shared/shared.module';
 import {LoginComponent} from './components/shared/login/login.component';
 import {HttpClientModule} from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { BeaconProvider } from './core/services/BeaconProvider.service';
+import { IBeacon } from '@ionic-native/ibeacon/ngx';
+import { LogicaDeNegocioFake } from './core/services/LogicaDeNegocioFake.service';
+import { LocalizadorGPS } from './core/services/LocalizadorGPS.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +31,13 @@ import {HttpClientModule} from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation,
+    LocalizadorGPS,
+    ReceptorBLE,
+    LogicaDeNegocioFake,
+    HttpClientModule,
+    IBeacon, BeaconProvider,
   ],
   bootstrap: [AppComponent]
 })
