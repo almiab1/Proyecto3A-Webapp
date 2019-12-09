1a11eb5dbbb2e52cb22fae963de91bdb00189843
oscarblanq <oscarblanq@gmail.com>
2019-12-09 11:56:50 +0100

    Widgets redimensionados en la Home

--- iPollution/src/app/pages/ciudadano/home/home.page.ts
+++ iPollution/src/app/pages/ciudadano/home/home.page.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {LoginService} from '../../../core/services/login.service';
 import {Platform} from '@ionic/angular';
+import {DataService} from "../../../core/services/data.service";
 
 @Component({
   selector: 'app-home',
@@ -8,22 +9,25 @@ import {Platform} from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  rolUser: number;
+  @Input() rolUser: number;
+  tamanyoWidget: number;
   constructor(private loginService: LoginService,
-              private platform: Platform) {
-    // this.rolUser = loginService.rolUser;
-    this.rolUser = 0;
+              private platform: Platform,
+              private data: DataService) {
+    this.rolUser = data.rolUser;
     this.contarNumeroWidgets();
   }
   ngOnInit() {
+    this.tamanyoWidget = this.contarNumeroWidgets();
   }
-  contarNumeroWidgets() {
-    let contador = 0;
+  contarNumeroWidgets(): number {
+    let contador = 1;
     if (this.platform.is('mobile')) {
       contador++;
     }
     if (this.rolUser === 1 || this.rolUser === 2) {
       contador++;
     }
+    return 12 / contador;
   }
 }
