408d1ad80d20fb54b42fd8ff6eb57d69855f1236
CTortosaM <cartormi@epsg.upv.es>
2020-01-02 22:30:31 +0100

    modulo de historico añadido

---
+++ iPollution/src/app/pages/administrador/historico/historico.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HistoricoComponent } from './historico.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HistoricoComponent]
+})
+export class HistoricoModule { }
