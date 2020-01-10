d79d6cc4c1a873eba48973bdf3a241e1daa7d410
oscarblanq <oscarblanq@gmail.com>
2020-01-10 13:10:53 +0100

    Botones añadidos administracion nodos

--- iPollution/src/app/pages/administrador/nodos/nodos.module.ts
+++ iPollution/src/app/pages/administrador/nodos/nodos.module.ts
@@ -6,7 +6,7 @@ import { IonicModule } from '@ionic/angular';
 
 
 import { NodosPage } from './nodos.page';
-import {Router, RouterModule} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {SharedModule} from '../../../components/shared/shared.module';
 
 import {AdminModule} from '../../../components/admin/admin.module';
