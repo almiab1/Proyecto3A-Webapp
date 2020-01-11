c00ae9419a9072c0075f7d934b730abc13013b84
Almiab1 <almiab1@epsg.upv.es>
2020-01-11 19:10:55 +0100

    funciona rutas previas con la bd en apartados rutas

--- iPollution/src/app/components/admin/modal-rutas/modal-rutas.component.ts
+++ iPollution/src/app/components/admin/modal-rutas/modal-rutas.component.ts
@@ -1,3 +1,4 @@
+import { DataService } from './../../../core/services/data.service';
 // ----------------------------------------------------------------------------------------------
 // modal-rutas.component.ts
 // Controlador modal rutas
@@ -29,6 +30,10 @@ import {
 import {
   MapaService
 } from '../../../core/services/Mapa.service';
+import {
+  Ruta,
+  RutasPreviamenteCreadas
+} from '../../../models/Rutas';
 // ----------------------------------------------------------------------------------------------
 // Components
 // ----------------------------------------------------------------------------------------------
@@ -43,6 +48,7 @@ import {
 export class ModalRutasComponent implements OnInit {
 
   // Propiedades
+  platform: string;
   tituloComponent: string;
   tipoModal: string;
   mapa: MapaService;
@@ -50,15 +56,24 @@ export class ModalRutasComponent implements OnInit {
     static: false
   }) mapElement: ElementRef;
 
+
+  //Rutas
+  currentMap = null;
+  rutasPreviasRealizadas: Ruta[] = [];
+  rutaSeleccionadaTiempo: any;
+  rutaSeleccionadaPredefinida: any;
+  rutasPredefinidas: RutasPreviamenteCreadas[];
+
   // Constructor
   constructor(
     private modalController: ModalController,
     private navParams: NavParams,
     private server: LogicaDeNegocioFake,
-    public gps: LocalizadorGPS
+    public gps: LocalizadorGPS,
+    public dataService: DataService
 
   ) {
-
+    this.platform = this.dataService.platform;
   }
 
   ngOnInit() {
@@ -68,40 +83,193 @@ export class ModalRutasComponent implements OnInit {
 
   ngAfterViewInit(): void {
     this.gps.obtenerMiPosicionGPS().then((resp) => {
-        this.gps.lat = resp.lat;
-        this.gps.long = resp.long;
+      this.gps.lat = resp.lat;
+      this.gps.long = resp.long;
 
-        this.mapa = new MapaService({
-          lat: resp.lat,
-          lng: resp.long
-        }, {
-          zoom: 15
-        }, this.mapElement.nativeElement);
-      })
-    }
+      this.mapa = new MapaService({
+        lat: resp.lat,
+        lng: resp.long
+      }, {
+        zoom: 15
+      }, this.mapElement.nativeElement);
+    })
+  }
 
-    // ----------------------------------------------------------------------------
-    // tipoBoton --> closeModal()
-    async closeModal(tipoBoton: string) {
-      const onClosedData = 'ModalCerrado';
+  // ----------------------------------------------------------------------------------------------
+  // tipoBoton --> closeModal()
+  // ----------------------------------------------------------------------------------------------
+  async closeModal(tipoBoton: string) {
+    const onClosedData = 'ModalCerrado';
 
-      switch (tipoBoton) {
-        // case 'guardar': {
-        //   console.log('----------Boton guardar modal------------');
+    switch (tipoBoton) {
+      // case 'guardar': {
+      //   console.log('----------Boton guardar modal------------');
 
-        //   this.serve.darDeAltaUsuario(user);
+      //   this.serve.darDeAltaUsuario(user);
 
-        //   await this.modalController.dismiss(onClosedData);
-        //   break;
-        // }
-        default: {
-          console.log('----------Default------------');
+      //   await this.modalController.dismiss(onClosedData);
+      //   break;
+      // }
+      default: {
+        console.log('----------Default------------');
 
-          await this.modalController.dismiss(onClosedData);
-          break;
-        }
+        await this.modalController.dismiss(onClosedData);
+        break;
       }
     }
-    // ----------------------------------------------------------------------------
-
   }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // ruta: objeto --> showHistoryRoute()
+  // metodo para mostrar el historial de rutas realizadas
+  // ----------------------------------------------------------------------------------------------
+  showHistoryRoute(route) {
+    this.currentMap = this.mapa.pintarRuta(route, this.currentMap);
+    this.mapa.refrescarMapa();
+  }
+  // ----------------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------------------------
+  // loadHistoricRoutes()
+  // metodo para cargar de la bd las rutas ya realizadas
+  // ----------------------------------------------------------------------------------------------
+  loadHistoricRoutes() {
+    this.rutasPredefinidas = this.cargarRutasPreviamenteCreadas();
+    this.rutasPreviasRealizadas = this.cargarRutasPrevias();
+  }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // eliminarRuta()
+  // metodo para cargar de la bd las rutas predefinidas
+  // ----------------------------------------------------------------------------------------------
+  eliminarRuta(nombreRuta) {
+    this.server.eliminarRuta(nombreRuta);
+  }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // cargarRutasPreviamenteCreadas()
+  // metodo para cargar de la bd las rutas predefinidas
+  // ----------------------------------------------------------------------------------------------
+  cargarRutasPreviamenteCreadas() {
+    let rutas: RutasPreviamenteCreadas[] = [];
+
+    //Ruta test
+    rutas.push({
+      nombreRuta: 'Ruta Novelda',
+      puntoInicial: {
+        lat: 38.381392,
+        lng: -0.768067
+      },
+      wayPoints: [{
+        location: {
+          lat: 38.381723,
+          lng: -0.774593
+        }
+      }, {
+        location: {
+          lat: 38.384118,
+          lng: -0.774465
+        }
+      }],
+      puntoFinal: {
+        lat: 38.383905,
+        lng: -0.770708
+      }
+    });
+
+
+    this.server.getRutas(0, undefined).subscribe(
+      res => {
+        if (res.length != 0 || res != undefined) {
+          res.forEach(element => {
+
+            let rutaPrevia: RutasPreviamenteCreadas = {
+              nombreRuta: element.nombreRuta,
+              puntoInicial: element.ruta[0],
+              puntoFinal: element.ruta[element.ruta.length - 1],
+              wayPoints: [],
+            };
+
+            if (element.ruta.length >= 3) {
+              for (let i = 1; i <= element.ruta.length - 1; i++) {
+                rutaPrevia.wayPoints.push({
+                  location: element.ruta[i]
+                });
+              }
+            }
+
+            rutas.push(rutaPrevia);
+
+          });
+        }
+      },
+      err => console.log(err),
+    );
+
+    return rutas;
+  }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // cargarRutasPrevias()
+  // metodo para cargar de la bd las rutas ya hechas
+  // ----------------------------------------------------------------------------------------------
+  cargarRutasPrevias() {
+    let rutas: Ruta[] = [];
+
+    this.server.getRutas(1, 'canut@gmail.com').subscribe(
+      res => {
+        rutas = res;
+      },
+      err => console.log(err),
+    );
+
+    return rutas;
+  }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // onSelectRuta()
+  // metodo para controlar el select de rutas
+  // ----------------------------------------------------------------------------------------------
+  onSelectRuta() {
+    console.log('INICIO ONSELECTRUTA');
+    let ruta: any[];
+    this.rutasPreviasRealizadas.forEach(element => {
+      console.log(element)
+      if (element.nombreRuta == this.rutaSeleccionadaTiempo) {
+        ruta = element.ruta
+      }
+    });
+    this.showHistoryRoute(ruta);
+    console.log('FIN ONSELECTRUTA');
+  }
+  // ----------------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------------------
+  // onSelectRutaPredefinida()
+  // Metodo para seleccionar una ruta predefinida
+  // ----------------------------------------------------------------------------------------------
+  onSelectRutaPredefinida() {
+    let ruta: any;
+
+    this.rutasPredefinidas.forEach(element => {
+      if (element.nombreRuta === this.rutaSeleccionadaPredefinida) {
+        ruta = element;
+      }
+    });
+
+    this.mapa.calcularYMostrarRutasPredefinida(ruta);
+  }
+  // ----------------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------------------------
+  // Comparar los objetos de rutas
+  // ----------------------------------------------------------------------------------------------
+  compareById(o1, o2) {
+    return o1.finished === o2.finished
+  }
+  // ----------------------------------------------------------------------------------------------
+
+}
